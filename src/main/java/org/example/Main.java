1ee691358375ef4fa3dfdded377cab2cfad58aa5
raushankumarbaitha <raushanbaitha12@gmail.com>
2025-04-20 09:32:49 +0530

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Apun hai Maven aur Apun aab kaam karne lagega");
+    }
+}
